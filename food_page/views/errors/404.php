72362e5f512922b8431a2296c2d0fdf34167b0a5
Stien-arch-cs50 <c35e0m@gmail.com>
2026-05-18 13:22:49 +0600

    commit 2

--- food_page/views/errors/404.php
+++
@@ -1,7 +0,0 @@
-<section class="panel">
-  <p class="eyebrow">404</p>
-  <h1>Page not found</h1>
-  <p>The page you requested does not exist.</p>
-  <a class="button primary" href="<?= e(url('home')) ?>">Back to home</a>
-</section>
-
--- food_page/views/errors/database.php
+++
@@ -1,7 +0,0 @@
-<section class="panel">
-  <p class="eyebrow">Database</p>
-  <h1>Connection problem</h1>
-  <p>Check your database name, credentials, and imported schema.</p>
-  <pre class="code-block"><?= e($message ?? '') ?></pre>
-</section>
-
